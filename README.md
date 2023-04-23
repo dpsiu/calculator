9da3d1dd05b0cff53a820e31f6907a77789d0992
dpsiu <denversiu24@gmail.com>
2023-04-22 20:31:23 -0500

    Post Project Reflection & JS Clean up

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ The purpose of this project aims for us to demonstrate all we know with foundati
 
 Proper understanding of this project will create a solid foundation to build on top off.
     For this reason, I am truly excited.
+
+POST-PROJECT REFLECTION
+This was an extreme stress of what I had learned so far. Lessons learned are - 
+- Practice as much as possible before hand when doing projects
+- Break down logic on paper/rubber duck
+- Deep understanding of methods would have aided this project
+Honestly, this felt rough. Although mentally taxing on a 
+    capability and imposter-syndrome front, I was able to code efficiently. 
+Big tasks that follow are remembering how I understood what I did in this project.
+Understanding the methods, syntax, available tools, etc.
--- app.js
+++ app.js
@@ -2,10 +2,6 @@ let firstNum = '';
 let operator = '';
 let currentNum = '';
 
-
-//While firstNum = '';, function numberBtn. 
-
-
 let workingExpression = document.getElementById("workingExpression");
 
 // [ AC ]Clears workingExpression string
@@ -34,8 +30,6 @@ const numberBtn = document.getElementsByClassName("numberBtn");
         })
     }
 
-
-
 // [ operatorBtns ] Assigns operator functionality. Finalizes "firstNum," begins collecting currentNum
 const operatorBtn = document.getElementsByClassName("operatorBtn");
     for (let i = 0; i < operatorBtn.length; i++){
@@ -48,21 +42,12 @@ const operatorBtn = document.getElementsByClassName("operatorBtn");
         })
     }
 
-/*
-const update = function (){
-    console.log(firstNum)
-    console.log(operator)
-    console.log(currentNum)
-}
-*/
-
 const equal = document.getElementById("equal");
 equal.addEventListener('click', function(){
     calculate()
 
     workingExpression.textContent = firstNum;
 })
-
 
 function calculate(){
     let afterOperator = workingExpression.textContent.indexOf(operator);
@@ -85,39 +70,3 @@ function calculate(){
 
         console.log(currentNum)
 }
-
-
-
-
-
-
-/* [ Equal/operate ] Equal functionality.
-    Take the working expression as a string. Identify wherever it has any of the operators.
-    Split the string at those points. Ex, 1 + 2 x 4 / 8 will equal ("1", "2", "4", "8"). Not quite what we want.
-    Need to replace the operator strings with the actual operation methods. Need to also return all the string elements as an int.
-    workingExpression.replace("characterToBeReplaced", "What to be replaced with"). Replace ("X", *) << would this work?
-    If so, I get "1", +, "2", *, "4", / "8"
-    Then need to turn strings into int.
-*/
-
-function add(){
-
-    console.log();
-}
-
-function subtract(){
-
-    console.log();  
-}
-
-function multiply(){
-
-    console.log();
-}
-
-function divide(){
-
-    console.log();
-}
-
-console.log();
